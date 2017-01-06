c31ff95d9279194406f641cc1a96236ad7c0b240
jgliss <jg@nilu.no>
2017-01-06 14:37:57 +0100

    Mocking out numpy in conf.py (docs)

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,21 @@ import shlex
 
 sys.path.insert(0, os.path.abspath('../'))
 
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = [
+    'numpy',
+]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
