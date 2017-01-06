872f9155d790a0755c330a1eb05b643cffdd585a
jgliss <jg@nilu.no>
2017-01-06 18:05:06 +0100

    Include __version__ attribute in package and change conf.py file in docs, such that latest version is set on build

--- pydoas/__init__.py
+++ pydoas/__init__.py
@@ -1,43 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Initiation of pydoas library
+from pkg_resources import get_distribution
 
-.. moduleauthor:: Jonas Gliß <jg@nilu.no>
+__version__ = get_distribution('pydoas').version
 
-.. todo:
-    
-    Future ideas:
-    
-    #. Spectral results and Analysis (:mod:analysis`)
-        1. Currently only DOASIS resultfile support
-        #. Include "scans" (e.g. for MAX DOAS measurements)
-            1. Defined by Start/Stop of individual FitResultFiles
-        #. Convenience functionality for scans
-            #. All scans in time interval
-            #. All data with species X (e.g. SO2) larger / smaller than thresh 
-            (e.g. 1E+18 cm-2)
-        #. Plotting
-            1. Diurnal profiles (e.g. BrO/SO2)
-            #. Individual scans (data vs. Elevation / Azimuth /plume age)
-                1. Plume cross section 
-                #. Plume evolution
-            #. Standard time series plotting
-            #. Scatter plot
-                1. start, stop
-                # additional conditioning (e.g. SO2 > ..., plume age > ..., 
-                above detection limit)
-            
-        #. Include more input routines for reading results :class:`SpectralResults`
-            1. csv, txt, etc..
-            #. Autoidentify data and start, stop
-        
-    #. UNCERTAINTY treatment
-       
-"""
-#==============================================================================
-# import os, so2cam
-# moduleBasePath=os.path.dirname(so2cam.__file__)
-#==============================================================================
 from os.path import abspath, dirname
 
 _LIBDIR = abspath(dirname(__file__))
