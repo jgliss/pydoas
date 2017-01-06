9b47ebbefea467ec9e894ded14bf88a8e22c4395
jgliss <jg@nilu.no>
2017-01-06 14:45:31 +0100

    Rolling back mocking out in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,9 @@ class Mock(MagicMock):
 MOCK_MODULES = [
     'numpy',
     'pandas'
+    'matplotlib'
 ]
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
